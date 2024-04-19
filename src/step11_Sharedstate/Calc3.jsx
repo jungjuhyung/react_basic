8e312294ef87d874efd4e1204473c2b9dff5b496
wngud01 <ramyun1537@gmail.com>
2024-04-19 18:20:51 +0900

    0419react2

--- src/step11_Sharedstate/Calc3.jsx
+++ src/step11_Sharedstate/Calc3.jsx
@@ -23,13 +23,14 @@ function TemperatureInput(props){
               type="number"
               value={props.temperature}
               onChange={handleChange} />
-         
         </fieldset>
     )
 }
+
 function toCelsius(fahrenheit){
   return((fahrenheit - 32) * 5 ) / 9;
 }
+
 function toFahrenheit(celsius){
     return(celsius * 9 ) / 5 + 32;
 }
@@ -39,20 +40,25 @@ function tryConvert(temperature, convert){
 }
 
 function Calc3(props) {
+    // 입력값에 따라 번하는 온도와 화씨, 섭씨 state 지정
     const [temperature, setTemperature] = useState('');
     const [scale, setScale] = useState('');
 
-    // 화씨 , 섭씨 
+    // input에 입력된 화씨 온도 값이 변할 때마다 state 변경
+    // 해당 함수는  TemperatureInput의 props로 넘어가서 TemperatureInput에서 실행된다.
     const handleCelsiusChange = (temperature) =>{
         setTemperature(temperature);
         setScale("c");
     }
-
+    
+    // input에 입력된 섭씨 온도 값이 변할 때마다 state 변경
+    // 해당 함수는  TemperatureInput의 props로 넘어가서 TemperatureInput에서 실행된다.
     const handleFahrenheitChange = (temperature) =>{
         setTemperature(temperature);
         setScale("f");
     }
 
+    // 실제 
     const celsius = scale ==="f" ? tryConvert(temperature, toCelsius) : temperature ;
     const fahrenheit = scale === "c"? tryConvert(temperature, toFahrenheit) : temperature ;
 
@@ -64,6 +70,5 @@ function Calc3(props) {
         </div>
     )
 }
-
 
 export default Calc3 ;
